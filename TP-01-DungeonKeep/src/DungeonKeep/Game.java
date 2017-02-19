e2d13ab43479c99fc69c187602e7f74639ee7c67
danrpinho <up201505302@fe.up.pt>
2017-02-19 16:36:50 +0000

    Added calculateNewX and calculateNewY functions.

--- TP-01-DungeonKeep/src/DungeonKeep/Game.java
+++ TP-01-DungeonKeep/src/DungeonKeep/Game.java
@@ -33,25 +33,23 @@ public class Game {
 		return direction;
 	}
 
-	
-
 	public boolean checkMove(int movement, int x, int y, Board b, int level) {
 		boolean result = true;
 
 		switch (movement) {
-		case 1:	//cima
+		case 1: // cima
 			if (b.board[level][x][y - 1] == 'X' || b.board[level][x][y - 1] == 'I')
 				result = false;
 			break;
-		case 2:	//baixo
+		case 2: // baixo
 			if (b.board[level][x][y + 1] == 'X' || b.board[level][x][y + 1] == 'I')
 				result = false;
 			break;
-		case 3:	//esq
+		case 3: // esq
 			if (b.board[level][x - 1][y] == 'X' || b.board[level][x - 1][y] == 'I')
 				result = false;
 			break;
-		case 4:	//dir
+		case 4: // dir
 			if (b.board[level][x + 1][y] == 'X' || b.board[level][x + 1][y] == 'I')
 				result = false;
 			break;
@@ -62,7 +60,7 @@ public class Game {
 
 		return result;
 	}
-	
+
 	public boolean checkIfLose(Board board, int level) {
 		boolean result = false;
 		for (int i = 0; i < board.entities.size(); i++) {
@@ -74,6 +72,52 @@ public class Game {
 					result = true;
 			}
 
+		}
+
+		return result;
+	}
+
+	public int calculateNewX(int movement, int x) {
+		int result;
+		switch (movement) {
+		case 1: // cima
+			result = x;
+			break;
+		case 2: // baixo
+			result = x;
+			break;
+		case 3: // esq
+			result = x - 1;
+			break;
+		case 4: // dir
+			result = x + 1;
+			break;
+		default:
+			result = x;
+			break;
+		}
+
+		return result;
+	}
+
+	public int calculateNewY(int movement, int y) {
+		int result;
+		switch (movement) {
+		case 1: // cima
+			result = y - 1;
+			break;
+		case 2: // baixo
+			result = y + 1;
+			break;
+		case 3: // esq
+			result = y;
+			break;
+		case 4: // dir
+			result = y;
+			break;
+		default:
+			result = y;
+			break;
 		}
 
 		return result;
