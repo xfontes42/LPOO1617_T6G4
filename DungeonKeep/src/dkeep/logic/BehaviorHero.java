aa084ac08e3e377b346fb7631c2d89f92011ea40
danrpinho <up201505302@fe.up.pt>
2017-02-24 11:54:20 +0000

    Added behavior files.

---
+++ DungeonKeep/src/dkeep/logic/BehaviorHero.java
@@ -0,0 +1,9 @@
+package dkeep.logic;
+
+public class BehaviorHero extends Behavior {
+
+	public BehaviorHero() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ DungeonKeep/src/dkeep/logic/BehaviorOgre.java
@@ -0,0 +1,9 @@
+package dkeep.logic;
+
+public class BehaviorOgre extends Behavior {
+
+	public BehaviorOgre() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
