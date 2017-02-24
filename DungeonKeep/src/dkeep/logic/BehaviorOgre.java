1298dcad787bd0fb67d2f13365849c505b40bdbd
xfontes <up201503145@fe.up.pt>
2017-02-24 12:58:06 +0000

    Update.

--- DungeonKeep/src/dkeep/logic/BehaviorOgre.java
+++ DungeonKeep/src/dkeep/logic/BehaviorOgre.java
@@ -1,9 +1,16 @@
 package dkeep.logic;
 
+import java.util.Random;
+
 public class BehaviorOgre extends Behavior {
 
 	public BehaviorOgre() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public int movement(){
+		Random random = new Random();
+		int result = random.nextInt(4) + 1;
+		return result;
+	}
 }
