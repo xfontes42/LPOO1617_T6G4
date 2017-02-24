23e3e6774d273d5a90714984df1e4d118c7f3d71
xfontes <up201503145@fe.up.pt>
2017-02-24 12:18:01 +0000

    Started working on the Guard Behavior.

--- DungeonKeep/src/dkeep/logic/Behavior.java
+++ DungeonKeep/src/dkeep/logic/Behavior.java
@@ -1,5 +1,12 @@
 package dkeep.logic;
 
 public class Behavior {
-
+	
+	public Behavior(){
+		
+	}
+	
+	public int movement(){
+		return 0;
+	}
 }
--- DungeonKeep/src/dkeep/logic/BehaviorGuard.java
+++ DungeonKeep/src/dkeep/logic/BehaviorGuard.java
@@ -2,8 +2,17 @@ package dkeep.logic;
 
 public class BehaviorGuard extends Behavior {
 
+	private int moves_pre2etermine2[] = { 3, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 2, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1 };
+	private int nextMove = 0;
+	
 	public BehaviorGuard() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public int movement(){
+		int result = moves_pre2etermine2[nextMove];
+		nextMove = (nextMove+1)%moves_pre2etermine2.length;
+		return result;
 	}
 
 }
