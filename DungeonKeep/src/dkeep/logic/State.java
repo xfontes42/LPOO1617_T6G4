56ac5bfa6e0c6c851150e897949841f4a53e2965
xfontes <up201503145@fe.up.pt>
2017-02-24 12:43:14 +0000

    Added some things to State class.

--- DungeonKeep/src/dkeep/logic/State.java
+++ DungeonKeep/src/dkeep/logic/State.java
@@ -1,5 +1,16 @@
 package dkeep.logic;
 
-public class State {
+import java.util.Vector;
 
+import DungeonKeep.Door;
+import DungeonKeep.Entity;
+import DungeonKeep.Hero;
+import DungeonKeep.Key;
+
+public class State {
+	char[][] board = new char[10][10];
+	public Vector<Entity> entities = new Vector<Entity>();
+	public Vector<Door> doors = new Vector<Door>();
+	public Hero hero = new Hero();
+	public Key key = new Key();
 }
