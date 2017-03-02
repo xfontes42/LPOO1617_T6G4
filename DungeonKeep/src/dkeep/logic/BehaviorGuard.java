112755a66287c28f5f3633d812867dac428d4fdf
danrpinho <up201505302@fe.up.pt>
2017-03-02 10:12:48 +0000

    Started implementing reversing direction mechanisms.

--- DungeonKeep/src/dkeep/logic/BehaviorGuard.java
+++ DungeonKeep/src/dkeep/logic/BehaviorGuard.java
@@ -1,9 +1,12 @@
 package dkeep.logic;
 
+import java.util.Random;
+
 public class BehaviorGuard extends Behavior {
 
 	private int moves_pre2etermine2[] = { 3, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 2, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1 };
 	private int nextMove = 0;
+	private boolean reversing = false;
 	
 	public BehaviorGuard() {
 		
@@ -13,6 +16,44 @@ public class BehaviorGuard extends Behavior {
 		int result = moves_pre2etermine2[nextMove];
 		nextMove = (nextMove+1)%moves_pre2etermine2.length;
 		return result;
+	}
+	
+	public int reverseDirection(int dir){
+		int result = 0;
+		
+		switch(dir){
+		case 1:
+			result = 2;
+			break;
+		case 2:
+			result = 1;
+			break;
+		case 3:
+			result = 4;
+			break;
+		case 4:
+			result = 3;
+			break;
+		case 5:
+			result = 5;
+			break;
+		default:
+			result = 0;
+			break;
+		}
+		
+		return result;
+	}
+	
+	public boolean setReverse(int prob){
+		
+		Random rand = new Random();
+		int x = rand.nextInt(prob);
+		
+		if (x == 0)
+			return true;
+		else
+			return false;
 	}
 
 }
