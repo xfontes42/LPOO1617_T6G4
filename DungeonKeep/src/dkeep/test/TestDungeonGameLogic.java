b772442e7d17d37192d339f26caeb7f4d6bba6a3
danrpinho <up201505302@fe.up.pt>
2017-03-03 10:58:51 +0000

    Implemented more tests.

--- DungeonKeep/src/dkeep/test/TestDungeonGameLogic.java
+++ DungeonKeep/src/dkeep/test/TestDungeonGameLogic.java
@@ -23,25 +23,44 @@ public class TestDungeonGameLogic {
 		assertEquals(1, game.hero.getX());
 		assertEquals(1, game.hero.getY());
 		
-		game.hero.moveEntity(2);//moves down
+		//tries moving down
+		if (game.checkMove(2, game.hero.getX(), game.hero.getY())){
+			game.hero.moveEntity(2);
+		}		
 		
 		game.updateEntity(game.hero.sprite, 1, 1, game.hero.getX(), game.hero.getX());
 		assertEquals(1, game.hero.getX());
 		assertEquals(2, game.hero.getY());
 	}
 	
-//	@Test
-//	public void testMoveHeroToWall(){
-//		
-//		
-//	}
-//	
-//	@Test
-//	public void testHeroCapturedByGuard(){
-//		
-//		
-//	}
-//	
+	@Test
+	public void testMoveHeroToWall(){
+		State game = new State(testMap);
+		game.startEntities();
+		
+		//tests correct starting position (1,1)
+		assertEquals(1, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		
+		//tries moving up
+		if (game.checkMove(1, game.hero.getX(), game.hero.getY())){
+			game.hero.moveEntity(1);
+		}
+		
+		assertEquals(1, game.hero.getX());
+		assertEquals(1, game.hero.getY());		
+	}
+	
+	@Test
+	public void testHeroCapturedByGuard(){
+		State game = new State(testMap);
+		game.startEntities();
+		
+		
+		
+		
+	}
+	
 //	@Test
 //	public void testHeroRunsIntoClosedDoor(){
 //		
