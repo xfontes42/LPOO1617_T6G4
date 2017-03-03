aeb71122f5dd2b27a3ef6925957ea08be45733e7
danrpinho <up201505302@fe.up.pt>
2017-03-03 11:10:20 +0000

    Implemented more tests.

--- DungeonKeep/src/dkeep/test/TestDungeonGameLogic.java
+++ DungeonKeep/src/dkeep/test/TestDungeonGameLogic.java
@@ -12,7 +12,7 @@ public class TestDungeonGameLogic {
 						{'X','H',' ','k','X'},
 						{'X',' ',' ',' ','X'},
 						{'X','G',' ',' ','X'},
-						{'X','X','I','I','X'}};
+						{'X','X','X','X','X'}};
 	
 	@Test
 	public void testMoveHeroToFreeCell(){
@@ -47,6 +47,7 @@ public class TestDungeonGameLogic {
 			game.hero.moveEntity(1);
 		}
 		
+		game.updateEntity(game.hero.sprite, 1, 1, game.hero.getX(), game.hero.getX());
 		assertEquals(1, game.hero.getX());
 		assertEquals(1, game.hero.getY());		
 	}
@@ -56,17 +57,44 @@ public class TestDungeonGameLogic {
 		State game = new State(testMap);
 		game.startEntities();
 		
+		//tries moving right (where the guard can catch them)
+		if (game.checkMove(4, game.hero.getX(), game.hero.getY())){
+			game.hero.moveEntity(4);
+		}
 		
+		game.updateEntity(game.hero.sprite, 1, 1, game.hero.getX(), game.hero.getX());
+		assertEquals(2, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		assertTrue(game.checkIfLose());
 		
 		
 	}
 	
-//	@Test
-//	public void testHeroRunsIntoClosedDoor(){
-//		
-//		
-//	}
-//	
+	@Test
+	public void testHeroRunsIntoClosedDoor(){
+		
+		State game = new State(testMap);
+		game.startEntities();
+		
+		//tests correct starting position (1,1)
+		assertEquals(1, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		
+		//tries moving down
+		if (game.checkMove(2, game.hero.getX(), game.hero.getY())){
+			game.hero.moveEntity(2);
+		}		
+		
+		game.updateEntity(game.hero.sprite, 1, 1, game.hero.getX(), game.hero.getX());
+		
+		//tries going into the door
+		if (game.checkMove(3, game.hero.getX(), game.hero.getY())){
+			game.hero.moveEntity(3);
+		}	
+		assertEquals(1, game.hero.getX());
+		assertEquals(2, game.hero.getY());
+	}
+	
 //	@Test
 //	public void testHeroFlipsLever(){
 //		
