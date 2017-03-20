cc10740a3bd81357a8500b807ffaf5443528b6d7
danrpinho <up201505302@fe.up.pt>
2017-03-20 11:37:56 +0000

    Worked some more on the level editor.

--- DungeonKeep/src/dkeep/gui/LevelEditor.java
+++ DungeonKeep/src/dkeep/gui/LevelEditor.java
@@ -8,52 +8,67 @@ import javax.swing.JPanel;
 
 public class LevelEditor extends JFrame {
 	JFrame parent;
+	int height, width;		//window size
+	int buttonW, buttonH;	//button size
 	
 	public LevelEditor(JFrame parent) {
+		int boardX=10, boardY=10;
+		
+		buttonW = 75;
+		buttonH = 23;
+		height = 20 + 32 * boardY;
+		width = 30 + 32 * boardX + buttonW;
 		setResizable(false);
+		super.setBounds(0, 0, width, height);
 		this.parent = parent;
 		getContentPane().setLayout(null);
 		
-		JButton btnExitEditor = new JButton("Exit Editor");
+		JButton btnExitEditor = new JButton("Exit");
 		btnExitEditor.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				dispose();
 			}
 		});
-		btnExitEditor.setBounds(395, 287, 89, 23);
-		getContentPane().add(btnExitEditor);
 		
-		JButton btnSaveLevel = new JButton("Save Level");
-		btnSaveLevel.setBounds(395, 253, 89, 23);
+		//button coordinates
+		int buttonX = width-10-buttonW;
+		
+		btnExitEditor.setBounds(buttonX, height - 10 - buttonH, buttonW, buttonH);
+		getContentPane().add(btnExitEditor);
+
+		JButton btnSaveLevel = new JButton("Save");
+		btnSaveLevel.setBounds(buttonX, height - 2 * (10 - buttonH), buttonW, buttonH);
 		getContentPane().add(btnSaveLevel);
 		
 		JButton btnHero = new JButton("Hero");
-		btnHero.setBounds(395, 11, 89, 23);
+		btnHero.setBounds(buttonX, 10, buttonW, buttonH);
 		getContentPane().add(btnHero);
 		
 		JButton btnOgre = new JButton("Ogre");
-		btnOgre.setBounds(395, 45, 89, 23);
+		btnOgre.setBounds(buttonX, 10+10+buttonH, buttonW, buttonH);
 		getContentPane().add(btnOgre);
 		
 		JButton btnKey = new JButton("Key");
-		btnKey.setBounds(395, 79, 89, 23);
+		btnKey.setBounds(buttonX, 10+2*(10+buttonH), buttonW, buttonH);
 		getContentPane().add(btnKey);
 		
 		JButton btnDoor = new JButton("Door");
-		btnDoor.setBounds(395, 113, 89, 23);
+		btnDoor.setBounds(buttonX, 10+3*(10+buttonH), buttonW, buttonH);
 		getContentPane().add(btnDoor);
 		
 		JButton btnWall = new JButton("Wall");
-		btnWall.setBounds(395, 145, 89, 23);
+		btnWall.setBounds(buttonX, 10+4*(10+buttonH), buttonW, buttonH);
 		getContentPane().add(btnWall);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 239, 239);
-		getContentPane().add(panel);
-		
 		JButton btnGround = new JButton("Ground");
-		btnGround.setBounds(395, 179, 89, 23);
+		btnGround.setBounds(buttonX, 10+5*(10+buttonH), buttonW, buttonH);
 		getContentPane().add(btnGround);
+		
+		JPanel pnlBoard = new JPanel();
+		pnlBoard.setBounds(10, 10, 32*boardX, 32*boardY);
+		getContentPane().add(pnlBoard);
+		
+		
 	}
 	
 	@Override
