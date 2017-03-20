a1dd981954801c08e01ea57eacb6398bd8b1e7e6
danrpinho <up201505302@fe.up.pt>
2017-03-20 11:08:17 +0000

    Started work on the level editor UI.

--- DungeonKeep/src/dkeep/gui/LevelEditor.java
+++ DungeonKeep/src/dkeep/gui/LevelEditor.java
@@ -4,11 +4,13 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
 
 public class LevelEditor extends JFrame {
 	JFrame parent;
 	
 	public LevelEditor(JFrame parent) {
+		setResizable(false);
 		this.parent = parent;
 		getContentPane().setLayout(null);
 		
@@ -18,8 +20,40 @@ public class LevelEditor extends JFrame {
 				dispose();
 			}
 		});
-		btnExitEditor.setBounds(335, 227, 89, 23);
+		btnExitEditor.setBounds(395, 287, 89, 23);
 		getContentPane().add(btnExitEditor);
+		
+		JButton btnSaveLevel = new JButton("Save Level");
+		btnSaveLevel.setBounds(395, 253, 89, 23);
+		getContentPane().add(btnSaveLevel);
+		
+		JButton btnHero = new JButton("Hero");
+		btnHero.setBounds(395, 11, 89, 23);
+		getContentPane().add(btnHero);
+		
+		JButton btnOgre = new JButton("Ogre");
+		btnOgre.setBounds(395, 45, 89, 23);
+		getContentPane().add(btnOgre);
+		
+		JButton btnKey = new JButton("Key");
+		btnKey.setBounds(395, 79, 89, 23);
+		getContentPane().add(btnKey);
+		
+		JButton btnDoor = new JButton("Door");
+		btnDoor.setBounds(395, 113, 89, 23);
+		getContentPane().add(btnDoor);
+		
+		JButton btnWall = new JButton("Wall");
+		btnWall.setBounds(395, 145, 89, 23);
+		getContentPane().add(btnWall);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 239, 239);
+		getContentPane().add(panel);
+		
+		JButton btnGround = new JButton("Ground");
+		btnGround.setBounds(395, 179, 89, 23);
+		getContentPane().add(btnGround);
 	}
 	
 	@Override
@@ -27,5 +61,4 @@ public class LevelEditor extends JFrame {
 		super.dispose();
 		this.parent.setVisible(true);
 	}
-
 }
